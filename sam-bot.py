6c8c40ca9b2bf5c875a8626bdc0f0b69eecf057a
Soroush Mosaddad <mosaddad.soroush@pm.me>
2023-06-10 18:52:53 +1000

    fixed key

--- sam-bot.py
+++ sam-bot.py
@@ -8,12 +8,12 @@ ds = sam_bot.subreddit("DeathStranding")
 with open("replied.txt", "a+") as f:
     f.seek(0)
     replied = (f.read()).split("\n")
-    # add for body as well
-    # add object detection
+    # add for body
+    # add object detection - check url for img clues
     for news in ds.new(limit=25):
         if news.id not in replied:
-            if ("my" and "birthday") in (news.title).lower():
+            key = (news.title).lower()
+            if (("my" in key or "me" in key) and ("birthday" in key or "birth day" in key)):
                 f.write(news.id + "\n")
-                #print(news.title)
                 news.reply("Happy Birthday " + str(news.author) + "!" + "\n\n" + "Keep on keepin' on!")    
 
